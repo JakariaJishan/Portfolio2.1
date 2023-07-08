196bb99771cd1a9dd2eb9361e416cebf510bb2f5
JakariaJishan <jakariajishan987@gmail.com>
2023-07-08 15:35:25 +0600

    Add download cv option

--- src/components/sidebar/Profile.js
+++ src/components/sidebar/Profile.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BsArrowUpRight, BsGithub, BsLinkedin } from "react-icons/bs";
-import { SiMaildotru } from 'react-icons/si';
+import { SiMaildotru } from "react-icons/si";
 import { Link } from "react-router-dom";
 export const Profile = () => {
   return (
@@ -11,25 +11,35 @@ export const Profile = () => {
         className="w-12 h-12 rounded-full"
       />
       <div className="flex justify-center items-center text-xs gap-3">
-        <div >
-          <Link to='https://github.com/JakariaJishan' className="flex items-center gap-1 group text-gray-400 hover:text-white">
+        <div>
+          <Link
+            to="https://github.com/JakariaJishan"
+            className="flex items-center gap-1 group text-gray-400 hover:text-white"
+          >
             <BsGithub />
             Github
             <BsArrowUpRight className="transition ease-out duration-300 group-hover:-translate-y-1 text-md " />
           </Link>
         </div>
-        <div >
-          <Link to='https://www.linkedin.com/in/jakariajishan/' className="flex items-center gap-1 group text-gray-400 hover:text-white">
+        <div>
+          <Link
+            to="https://www.linkedin.com/in/jakariajishan/"
+            className="flex items-center gap-1 group text-gray-400 hover:text-white"
+          >
             <BsLinkedin />
             Linkedin
             <BsArrowUpRight className="transition ease-out duration-300 group-hover:-translate-y-1 text-md " />
           </Link>
         </div>
-        <div >
-          <Link to='#' onClick={(e) => {
-                window.location.href = "mailto:no-reply@example.com";
-                e.preventDefault();
-            }} className="flex items-center gap-1 group text-gray-400 hover:text-white">
+        <div>
+          <Link
+            to="#"
+            onClick={(e) => {
+              window.location.href = "mailto:no-reply@example.com";
+              e.preventDefault();
+            }}
+            className="flex items-center gap-1 group text-gray-400 hover:text-white"
+          >
             <SiMaildotru />
             Mail
             <BsArrowUpRight className="transition ease-out duration-300 group-hover:-translate-y-1 text-md " />
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,7 +1,7 @@
 import React from "react";
+import { LiaDownloadSolid } from 'react-icons/lia';
 import NavigateLink from "./NavigateLink";
 import { Profile } from "./Profile";
-
 export const Sidebar = () => {
   const links = [
     {
@@ -22,7 +22,7 @@ export const Sidebar = () => {
   ];
   return (
     <div className=" w-full">
-      <div className="flex flex-col h-screen justify-between">
+      <div className="flex flex-col h-screen justify-between py-10">
         <div>
           <h1 className="text-6xl">
             Hello, I'm <br /> Jakaria Jishan
@@ -45,6 +45,10 @@ export const Sidebar = () => {
         <div className="">
           <Profile/>
         </div>
+        <button class="transition ease-out bg-black hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 w-[118px] px-3 py-2 rounded-full text-xs flex items-center gap-2">
+          Download CV
+          <LiaDownloadSolid className="animate-ping"/>
+        </button>
       </div>
     </div>
   );
