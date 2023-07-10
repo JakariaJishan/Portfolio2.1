0e9d45a272a67131ca29f4b143506378121338b9
JakariaJishan <jakariajishan987@gmail.com>
2023-07-10 10:57:08 +0600

    Clear input field after submitting

--- src/components/contact/ContactPage.js
+++ src/components/contact/ContactPage.js
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsFillSendFill } from "react-icons/bs";
 export const ContactPage = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
   const handleSubmit = (e) => {
     e.preventDefault()
     alert('Your message successfully submitted!!!')
+    setEmail('')
+    setMessage('')
+    setName('')
   }
   return (
     <div className="mx-10 my-2">
       <h5 className="text-[#A0A8A4] font-bold text-md pt-10 pb-5  bg-[#151515] sticky top-0 z-50">
         Get In Touch
       </h5>
-      <form className="  mt-5">
+      <form className="mt-5" onSubmit={handleSubmit}>
         <div className="p-10  bg-[#1B1B1D] ">
           <div className="">
             <label>Full Name</label>
             <input
               type="text"
               name="first-name"
+              value={name}
               id="first-name"
               autoComplete="given-name"
               required
+              onChange={(e)=>setName(e.target.value)}
               className="my-2 block bg-transparent w-full border-0 border-b border-grey-dark py-1.5 text-white shadow-sm  placeholder:text-white sm:text-sm sm:leading-6   focus:ring-0 focus:outline-none"
             />{" "}
           </div>
@@ -29,6 +37,8 @@ export const ContactPage = () => {
               type="email"
               name="first-name"
               id="first-name"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
               autoComplete="given-name"
               required
               className="my-2 block bg-transparent w-full border-0 border-b border-grey-dark py-1.5 text-white shadow-sm  placeholder:text-white sm:text-sm sm:leading-6   focus:ring-0 focus:outline-none"
@@ -39,6 +49,8 @@ export const ContactPage = () => {
             <textarea
               name="first-name"
               id="first-name"
+              value={message}
+              onChange={e=>setMessage(e.target.value)}
               rows={3}
               autoComplete="given-name"
               required
@@ -47,7 +59,7 @@ export const ContactPage = () => {
           </div>
           <div className="flex justify-center ">
             <button className="mt-8 flex justify-center items-center gap-2 bg-black transition ease-out bg-black hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 px-5 py-2"
-            onClick={handleSubmit}
+            
             >
               <p className="">Submit</p>
               <BsFillSendFill />
